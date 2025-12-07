f1b5bea0db8f77db257de38f51e0bc603d25d76b
Michelle Berthiaume <michelle.berthiaume@atlasschool.com>
2025-12-06 19:22:23 -0600

    starting structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
 import "./index.css";
 
 // Render the App component to the root element
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
